b2408b8c258c796474d2a6487765c276e9c478f1
王璐瑶 <1347888240@qq.com>
2019-07-30 13:20:51 +0800

    修改信息

--- src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "你好！")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
